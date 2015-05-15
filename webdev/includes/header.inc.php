faeabe87d6576cdf0f7c48ce38fb5a9ab49d7229
teamgreen <teamgreen@users.noreply.github.com>
2015-05-15 11:36:13 -0700

    Create header.inc.php

---
+++ webdev/includes/header.inc.php
@@ -0,0 +1,19 @@
+
+		<header>
+			<div class="logo">
+				<a href="#" title="Home">
+					<img src="images/greenwell.jpg" alt="Greenwell Logo">
+				</a>
+			</div>
+			<div class="title">
+				<h1>Bid-Well Application</h1>
+			</div>
+			<div>
+				<ul class="top-nav">
+					<li><a href="#" title="Sign out">Sign out</a></li>
+					<li><a href="help.php" title="Help">Help</a></li>
+					<li><a href="about.php" title="About">About</a></li>
+					<li><a href="#" title="Greenwell">Greenwell</a></li>
+				</ul>
+			</div>
+		</header>
