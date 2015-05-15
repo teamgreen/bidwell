a6645163f067bba6e45695a7ac8ef9c7f18ec520
teamgreen <teamgreen@users.noreply.github.com>
2015-05-15 11:37:14 -0700

    Create footer.inc.php

---
+++ webdev/includes/footer.inc.php
@@ -0,0 +1,9 @@
+<footer>
+			<ul>
+				<li><a href="#" title="Greenwell">Greenwell</a></li>
+				<li><a href="#" title="About">About</a></li>
+				<li><a href="#" title="Help">Help</a></li>
+				<li><a href="#" title="Sign out">Sign out</a></li>
+			</ul>
+			<p>Product of Greenwell Bank &copy; 2015</p>
+		</footer>
