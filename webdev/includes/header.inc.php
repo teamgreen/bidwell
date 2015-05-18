adf23f2d30492458b7c6810bb0735ba98951263e
timbuk33 <t.willbanks@students.clark.edu>
2015-05-17 21:23:13 -0700

    Revert "Revert "Update header.inc.php""
    This reverts commit fc9dafc3a84003495abc0403d0d4d6d14bf954f5.
    
    Conflicts:
    	webdev/includes/header.inc.php

--- webdev/includes/header.inc.php
+++ webdev/includes/header.inc.php
@@ -1,12 +1,19 @@
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
 <?php 
 
 	echo "<header>\n";
 	echo "<div class=\"logo\">\n";
 	echo "<a href=\"#\" title=\"Home\">\n";
 	echo "<img src=\"images/greenwell.jpg\" alt=\"Greenwell Logo\" </a></div>\n";
+<<<<<<< HEAD
 	echo "<div class=\"title\">\n";
+=======
+	echo "<div class=\"title\">\n;
+>>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
 	echo "<h1>Bid-Well Application</h1>\n";
 	echo "<h2>Greenwell Bank</h2>\n";
 	echo "</div>\n";
@@ -21,6 +28,7 @@
 	echo "</header>\n";
 
 ?>
+<<<<<<< HEAD
 =======
 		<header>
 			<div class="logo">
@@ -42,3 +50,5 @@
 			</div>
 		</header>
 >>>>>>> fc9dafc3a84003495abc0403d0d4d6d14bf954f5
+=======
+>>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
