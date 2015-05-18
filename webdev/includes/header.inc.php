6501a38995b766bd375777b928c9ad82410d2687
Alexandrea R. Chaudoin <archaudoin@hotmail.com>
2015-05-17 18:15:56 -0700

    changed header.inc.php and stye.css

--- webdev/includes/header.inc.php
+++ webdev/includes/header.inc.php
@@ -1,20 +1,20 @@
 
-<header>  
-3  -			<div class="logo">  
-4  -				<a href="#" title="Home">  
-5  -					<img src="images/greenwell.jpg" alt="Greenwell Logo">  
-6  -				</a>  
-7  -			</div>  
-8  -			<div class="title">  
-9  -				<h1>Bid-Well Application</h1>  
-10  -				<h2>Greenwell Bank</h2>  
-11  -			</div>  
-12  -			<div>  
-13  -				<ul class="top-nav">  
-14  -					<li><a href="#" title="Sign out">Sign out</a></li>  
-15  -					<li><a href="help.php" title="Help">Help</a></li>  
-16  -					<li><a href="about.php" title="About">About</a></li>  
-17  -					<li><a href="#" title="Greenwell">Greenwell</a></li>  
-18  -				</ul>  
-19  -			</div>  
-20  -		</header>  
+		<header>  
+			<div class="logo">  
+				<a href="#" title="Home">  
+					<img src="images/greenwell.jpg" alt="Greenwell Logo">  
+				</a>  
+			</div>  
+			<div class="title">  
+				<h1>Bid-Well Application</h1>  
+				<h2>Greenwell Bank</h2>  
+			</div>  
+			<div>  
+				<ul class="top-nav">  
+					<li><a href="#" title="Sign out">Sign out</a></li>  
+					<li><a href="help.php" title="Help">Help</a></li>  
+					<li><a href="about.php" title="About">About</a></li>  
+					<li><a href="#" title="Greenwell">Greenwell</a></li>  
+				</ul>  
+			</div>  
+		</header>  
