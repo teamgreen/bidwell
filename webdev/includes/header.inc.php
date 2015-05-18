fc9dafc3a84003495abc0403d0d4d6d14bf954f5
timbuk33 <t.willbanks@students.clark.edu>
2015-05-17 17:51:59 -0700

    Revert "Update header.inc.php"
    This reverts commit a0614d81169f0e5eac1dc6885536650f07e7d42e.

--- webdev/includes/header.inc.php
+++ webdev/includes/header.inc.php
@@ -1,22 +1,20 @@
 
-<?php 
-
-	echo "<header>\n";
-	echo "<div class=\"logo\">\n";
-	echo "<a href=\"#\" title=\"Home\">\n";
-	echo "<img src=\"images/greenwell.jpg\" alt=\"Greenwell Logo\" </a></div>\n";
-	echo "<div class=\"title\">\n;
-	echo "<h1>Bid-Well Application</h1>\n";
-	echo "<h2>Greenwell Bank</h2>\n";
-	echo "</div>\n";
-	echo "<div>\n";
-	echo "<ul class=\"top-nav\">\n";
-	echo "<li><a href=\"#\" title=\"Sign out\">Sign out</a></li>\n";
-	echo "<li><a href=\"help.php\" title=\"Help\">Help</a></li>\n";
-	echo "<li><a href=\"about.php\" title=\"About\">About</a></li>\n";
-	echo "<li><a href=\"#\" title=\"Greenwell\">Greenwell</a></li>\n";
-	echo "</ul>\n";
-	echo "</div>\n";
-	echo "</header>\n";
-
-?>
+		<header>
+			<div class="logo">
+				<a href="#" title="Home">
+					<img src="images/greenwell.jpg" alt="Greenwell Logo">
+				</a>
+			</div>
+			<div class="title">
+				<h1>Bid-Well Application</h1>
+				<h2>Greenwell Bank</h2>
+			</div>
+			<div>
+				<ul class="top-nav">
+					<li><a href="#" title="Sign out">Sign out</a></li>
+					<li><a href="help.php" title="Help">Help</a></li>
+					<li><a href="about.php" title="About">About</a></li>
+					<li><a href="#" title="Greenwell">Greenwell</a></li>
+				</ul>
+			</div>
+		</header>
