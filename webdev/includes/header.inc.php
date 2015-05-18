0f30bdf48c1fd1ea96f5d0afd8a4b05101cf2234
Alexandrea R. Chaudoin <archaudoin@hotmail.com>
2015-05-17 22:04:01 -0700

    fixed header

--- webdev/includes/header.inc.php
+++ webdev/includes/header.inc.php
@@ -1,36 +1,4 @@
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
-<?php 
-
-	echo "<header>\n";
-	echo "<div class=\"logo\">\n";
-	echo "<a href=\"#\" title=\"Home\">\n";
-	echo "<img src=\"images/greenwell.jpg\" alt=\"Greenwell Logo\" </a></div>\n";
-<<<<<<< HEAD
-	echo "<div class=\"title\">\n";
-=======
-	echo "<div class=\"title\">\n;
->>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
-	echo "<h1>Bid-Well Application</h1>\n";
-	echo "<h2>Greenwell Bank</h2>\n";
-	echo "</div>\n";
-	echo "<div>\n";
-	echo "<ul class=\"top-nav\">\n";
-	echo "<li><a href=\"#\" title=\"Sign out\">Sign out</a></li>\n";
-	echo "<li><a href=\"help.php\" title=\"Help\">Help</a></li>\n";
-	echo "<li><a href=\"about.php\" title=\"About\">About</a></li>\n";
-	echo "<li><a href=\"#\" title=\"Greenwell\">Greenwell</a></li>\n";
-	echo "</ul>\n";
-	echo "</div>\n";
-	echo "</header>\n";
-
-?>
-<<<<<<< HEAD
-=======
 		<header>
 			<div class="logo">
 				<a href="#" title="Home">
@@ -50,27 +18,3 @@
 				</ul>
 			</div>
 		</header>
->>>>>>> fc9dafc3a84003495abc0403d0d4d6d14bf954f5
-=======
->>>>>>> parent of fc9dafc... Revert "Update header.inc.php"
-=======
-		<header>  
-			<div class="logo">  
-				<a href="#" title="Home">  
-					<img src="images/greenwell.jpg" alt="Greenwell Logo">  
-				</a>  
-			</div>  
-			<div class="title">  
-				<h1>Bid-Well Application</h1>  
-				<h2>Greenwell Bank</h2>  
-			</div>  
-			<div>  
-				<ul class="top-nav">  
-					<li><a href="#" title="Sign out">Sign out</a></li>  
-					<li><a href="help.php" title="Help">Help</a></li>  
-					<li><a href="about.php" title="About">About</a></li>  
-					<li><a href="#" title="Greenwell">Greenwell</a></li>  
-				</ul>  
-			</div>  
-		</header>  
->>>>>>> origin/master
