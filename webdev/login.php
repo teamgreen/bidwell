e2b842f8789cc4ce9f6dafc60dfaa65f4d9a2755
timbuk33 <t.willbanks@students.clark.edu>
2015-05-20 13:29:43 -0700

    here goes

--- webdev/login.php
+++ webdev/login.php
@@ -3,24 +3,31 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Bid-Well Login Page</title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/login-style.css">
 </head>
 <body>
 <div class="wrapper">
-	<?php @require_once "includes/header-login.inc.php"; ?>
+	<?php @require_once "includes/login-header.inc.php"; ?>
 	<div class="content">
-		<h2>Login Page</h2>
-		<form method="post" action="#">
-			<label for="username">Username:</label>
-			<input type="text" name="username" tabindex=1 autofocus require><br>
-			<label for="company">Company:</label>
-			<input type="text" name="company" tabindex=2 require><br>
-			<label for="password">Password:</label>
-			<input type="text" name="password" tabindex=3 require><br><br>
-			<input type="submit" value="Login" tabindex=4><br><br>
-		</form>
-		<p><a href="#">Admin</a>&nbsp;&nbsp;<a href="main.php">Project</a></p>
-		<?php @require_once "includes/footer.inc.php"; ?>
+		<h2>Login Page</h2><br><br>
+		<div class="admin">
+			<div class="logo2">
+				<img src="images/greenwell.jpg">
+			</div>
+				<div class="form">
+					<form method="post" action="#">
+					<label for="username">Username:</label>
+					<input type="text" name="username" tabindex=1 autofocus require><br>
+					<label for="company">Company:</label>
+					<input type="text" name="company" tabindex=2 require><br>
+					<label for="password">Password:</label>
+					<input type="text" name="password" tabindex=3 require><br><br>
+					<input type="submit" value="Login" tabindex=4><br><br>
+					</form>
+				</div>
+		</div>
+		
+		<?php @require_once "includes/login-footer.inc.php"; ?>
 		
 	</div>
 	
