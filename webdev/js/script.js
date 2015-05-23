cd2566ea75ef2d37f1208f8e45693a9410e5244d
Alexandrea R. Chaudoin <archaudoin@hotmail.com>
2015-05-22 20:08:13 -0700

    changed table row colors in style.css, tuned up functions in script.js

--- webdev/js/script.js
+++ webdev/js/script.js
@@ -50,62 +50,29 @@ $(document).ready(function(){
 	// Authored by Alex Chaudoin
 	/////////////////////////////
 
-	// For both hover functions below:
-	// When the user hovers over one of
-	// the even or odd rows in the admin
-	// table in Tab A, the color will change
-	// to a darker color. 
-	// When the user hovers off any of the 
-	// rows, the color will return to normal.
-
-	$('#tab-a tr:nth-child(even)').hover(
-		function(){
-			$(this).css('background-color', '#d1f294');
-		}, 
-		function(){
-			$(this).css('background-color', '#dcf6ac');
-		}
-	); // end hover
-
-	$('#tab-a tr:nth-child(odd):not(:first-child)').hover(
-		function(){
-			$(this).css('background-color', '#f2fae3');
-		}, 
-		function(){
-			$(this).css('background-color', '#ffffff');
-		}
-	); // end hover
-
-	// In progress function below:
+	// Click function below:
 	// When clicking on a row in the admin table,
 	// that row will be highlighted and the 
 	// "New" button will change to "Edit".
-	// ISSUES --> Have not figured out how to only
-	//			  allow one row to be clicked on
-	//	          at a time without using just a
-	//			  a button instead.
-	//            THE FUNCTION ONLY WORKS WITH
-	//            THE FIRST CHANGED ROW.
 
 	$('#tab-a tr:not(:first-child)').click(function(){
 
-		var originalBg = $(this).css('background-color');
-		var changedRow = $(this);
-		changedRow.css('background-color', '#efefef');
-		$('.new-edit').attr('value', 'Edit');
-
-		changedRow.click(function(){
+		if ($('.new-edit').attr('value') === 'New') {
+			$(this).css('background-color', '#ffffff');
+			$('.new-edit').attr('value', 'Edit');
+		} else {
 			$('.new-edit').attr('value', 'New');
-			changedRow.css('background-color', originalBg);
-		})
+			$(this).css('background-color', '#dcf6ac');
+		};
+
 	}); // end click
+
 
 	// In progress function below:
 	// when the new/edit button is clicked,
 	// depending on the name of its "value",
 	// either the add-account or the 
 	// edit-account dialog will be opened.
-	// BUTTONS DONT WORK
 
 	var addDialog = $('#add-account').dialog({
 			autoOpen: false,
@@ -113,12 +80,12 @@ $(document).ready(function(){
 			width: 350,
 			modal: true,
 			buttons: { 
-				Save: function(){
-					dialog.dialog('close'); 
-					// need to add "add user" function
-				},
 				Cancel: function(){
-					dialog.dialog('close');
+					addDialog.dialog('close');
+				},
+				Save: function(){
+					addDialog.dialog('close'); 
+					// need to add "add user" function
 				}
 			}
 		}); // end dialog
@@ -129,12 +96,12 @@ $(document).ready(function(){
 			width: 350,
 			modal: true,
 			buttons: { 
-				Save: function(){
-					dialog.dialog('close'); 
-					// need to add "add user" function
-				},
 				Cancel: function(){
-					dialog.dialog('close');
+					editDialog.dialog('close');
+				},
+				Save: function(){
+					editDialog.dialog('close'); 
+					// need to add "add user" function
 				}
 			}
 		}); // end dialog
