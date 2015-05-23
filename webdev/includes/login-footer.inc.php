b6f23c787e9c7beb4d1b8069378677ae001575a0
Alexandrea R. Chaudoin <archaudoin@hotmail.com>
2015-05-23 15:26:14 -0700

    added links for debugging in footer files

--- webdev/includes/login-footer.inc.php
+++ webdev/includes/login-footer.inc.php
@@ -6,4 +6,14 @@
 				<li><a href="#" title="Sign out">Sign out</a></li>
 			</ul>
 			<p class="footer_copy">Product of Greenwell Bank &copy; 2015</p>
+		
+		<!-- Links added below for debugging, will remove later -->
+		<!-- Added by Alex Chaudoin -->
+			<ul>
+				<li><a href="home.php" title="home">Home</a></li>
+				<li><a href="admin.php" title="admin">Admin</a></li>
+				<li><a href="project.php" title="project">Project</a></li>
+			</ul>
+		<!-- End of links -->
+
 		</footer>
